70692bbe7de93a271105396c78166861d3056ed2
imindesign96 <phanngocvu89@gmail.com>
2021-07-22 15:07:40 +0900

    update js

---
+++ apps/script.js
@@ -0,0 +1,26 @@
+// About Tabs
+
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click",(e) =>{
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+});
+
+// Portfolio Item Details Popup
+
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPoup();
+    }
+})
+
+function togglePortfolioPoup(){
+    document.querySelector(".portfolio-group").classList.toggle("open");
+}
