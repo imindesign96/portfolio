17fed486a33d903dd0eb790023ac1c6b9030de7b
imindesign96 <phanngocvu89@gmail.com>
2021-07-31 01:17:32 +0900

    test

--- apps/script.js
+++ apps/script.js
@@ -18,37 +18,37 @@ aboutSection = document.querySelector(".about-section");
 // }
 
 /* --------------- ACTIVE SECTION ------------------- */
-document.addEventListener("click", (e) => {
-    navToggleer.classList.add("hide"); 
-    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
-        const hash = e.target.hash;
-        if(e.target.classList.contains("nav-item")){
-            toggleNavbar();
-        }
-        else{
-            hideSection();
-            document.body.classList.add("hide-scrolling");
-        }
-        setTimeout(() =>{
-            document.querySelector("section.active").classList.remove("active","fade-out");
-            document.querySelector(e.target.hash).classList.add("active");
-            window.scrollTo(0,0);
-            document.body.classList.remove("hide-scrolling");
-            navToggleer.classList.add("hide"); 
+// document.addEventListener("click", (e) => {
+//     navToggleer.classList.add("hide"); 
+//     if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+//         const hash = e.target.hash;
+//         if(e.target.classList.contains("nav-item")){
+//             toggleNavbar();
+//         }
+//         else{
+//             hideSection();
+//             document.body.classList.add("hide-scrolling");
+//         }
+//         setTimeout(() =>{
+//             document.querySelector("section.active").classList.remove("active","fade-out");
+//             document.querySelector(e.target.hash).classList.add("active");
+//             window.scrollTo(0,0);
+//             document.body.classList.remove("hide-scrolling");
+//             navToggleer.classList.add("hide"); 
 
-        },500)
-    }
-})
+//         },500)
+//     }
+// })
 
-tabsContainer.addEventListener("click",(e) =>{
-    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
-        tabsContainer.querySelector(".active").classList.remove("active");
-        e.target.classList.add("active");
-        const target = e.target.getAttribute("data-target");
-        aboutSection.querySelector(".tab-content.active").classList.remove("active");
-        aboutSection.querySelector(target).classList.add("active");
-    }
-});
+// tabsContainer.addEventListener("click",(e) =>{
+//     if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+//         tabsContainer.querySelector(".active").classList.remove("active");
+//         e.target.classList.add("active");
+//         const target = e.target.getAttribute("data-target");
+//         aboutSection.querySelector(".tab-content.active").classList.remove("active");
+//         aboutSection.querySelector(target).classList.add("active");
+//     }
+// });
 
 // Portfolio Item Details Popup
 
